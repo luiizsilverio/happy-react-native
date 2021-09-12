34ef5b0ecf4fa7acaeec1190e9958d6c240a7b08
luiizsilverio <luiiz.silverio@gmail.com>
2021-09-12 09:28:54 -0300

    Workshop 4 (0:43)

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Dimensions, View } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import markerPng from './src/images/marker.png'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}>      
+      <MapView 
+        style={styles.map} 
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: -22.9198944,
+          longitude: -47.067326,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}
+      >
+        <Marker
+          icon={markerPng}
+          coordinate={{
+            latitude: -22.9198944,
+            longitude: -47.067326,
+          }}
+        />
+      </MapView>      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1    
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  }
 });
