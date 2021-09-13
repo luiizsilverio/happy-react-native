6e8d08b976d4d98fce401fe81ebdb69126253360
luiizsilverio <luiiz.silverio@gmail.com>
2021-09-13 20:26:13 -0300

    Workshop 4 OK

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import { StyleSheet, Dimensions, View, Text, TouchableOpacity } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { useNavigation } from '@react-navigation/native'
+import { Feather } from '@expo/vector-icons'
+
+import markerPng from '../images/marker.png'
+
+export default function OrphanagesMap() {
+  const navigation = useNavigation()
+
+  function handleDetails() {
+    navigation.navigate('OrphanageDetails' as any)
+  }
+
+  return (
+    <View style={styles.container}>      
+      <MapView 
+        style={styles.map} 
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: -22.9198944,
+          longitude: -47.067326,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}
+      >
+        <Marker
+          icon={markerPng}
+          coordinate={{
+            latitude: -22.9198944,
+            longitude: -47.067326,
+          }}
+          calloutAnchor={{
+            x: 2.5,
+            y: 0.75
+          }}
+        >
+          <Callout tooltip={true} onPress={handleDetails}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>   
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+
+        <TouchableOpacity 
+          style={styles.createButton}
+          onPress={() => {}}
+        >
+          <Feather name="plus" size={20} color="#fff" />
+        </TouchableOpacity>
+      </View>   
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1    
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center'
+  },
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
+    fontFamily: 'Nunito_700Bold'
+  },
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    elevation: 1.5
+  },
+  footerText: {
+    color: '#8fa7b3',
+    fontFamily: 'Nunito_700Bold'
+  },
+  createButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
